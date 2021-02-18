74c53d190099d656c4130f452e5ff2c5a679df3d
Yuliya be-Code <yuliabochkovskaya@gmail.com>
2021-02-18 16:48:30 +0100

    changes

---
+++ script.js
@@ -0,0 +1,55 @@
+/**$.ajax({
+    url: 'http://remote_url?&callback=',
+    type: 'post',
+    dataType: 'jsonp',
+    success: function(result){
+        alert(result);
+    },
+    error: function(result){
+        alert(result);
+    }
+}); **/
+
+var ctx = document.getElementById('myChartOne').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
+/**
+
+var ctx = document.getElementById('myChartTwo').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+}); **/
+
