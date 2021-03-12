621de61ba902c1fee3c6b2b7e893b8c6352ce750
Yuliya Bochkovskaya <yuliabochkovskaya@gmail.com>
2021-03-12 16:52:23 +0100

    js  customized

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-
 function getTableData(query, labels = false) {
-  let json = { dataset: [] };
+  let json = {
+    dataset: []
+
+  };
 
   if (labels) {
     json.labels = [];
@@ -10,15 +12,38 @@ function getTableData(query, labels = false) {
   }
 
   document.querySelectorAll(query + ' tr:not(:first-child)').forEach(item => {
-    let obj = { label: '', data: [], backgroundColor: "green" };
+    let obj = {
+      label: '',
+      data: [],
+
+    };
+
+    //The JSON.stringify() method converts a JavaScript object or value to a JSON string
+    var myJSONstringifined = JSON.stringify(json.dataset)
+    console.log(typeof myJSONstringifined)
+
+    //The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string.
+    const toJSONobj = JSON.parse(myJSONstringifined);
+    console.log(typeof toJSONobj)
+    console.log(toJSONobj)
+
+    //To loop through an object array or just array in javascript, I should do the following
+    for (var i = 0; i < toJSONobj.length; i++) {
+      console.log(toJSONobj[i].label);
+     
+    }
+
 
     item.querySelectorAll('td').forEach(item => {
       if (!obj.label.length) obj.label = item.innerText
       else obj.data.push(parseFloat(item.innerText))
+
     })
     json.dataset.push(obj)
+
   })
   return json;
+
 }
 
 let table1data = getTableData('#table1', true)
@@ -34,17 +59,14 @@ let chart = new Chart(ctx, {
 
 });
 
-
-
-
 let table2data = getTableData('#table2')
 let ctx2 = document.getElementById('myChartTwo').getContext('2d');
 let chart2 = new Chart(ctx2, {
-  type: 'line',
+  type: 'bar',
   data: {
     labels: ['2007-09', '2010-12'],
-
     datasets: table2data.dataset,
+    
 
   }
 });
